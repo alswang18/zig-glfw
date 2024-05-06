0804a485201c6843542f700342367a5693aa4f1e
Alec Wang <alec.sy.wang@gmail.com>
2024-05-05 23:21:30 -0700

    initialized glfw setup

---
+++ src/main.zig
@@ -0,0 +1,73 @@
+const std = @import("std");
+const glfw = @import("mach-glfw");
+const gl = @import("gl");
+
+const glfw_log = std.log.scoped(.glfw);
+const gl_log = std.log.scoped(.gl);
+
+/// Procedure table that will hold loaded OpenGL functions.
+var gl_procs: gl.ProcTable = undefined;
+
+fn logGLFWError(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    glfw_log.err("{}: {s}\n", .{ error_code, description });
+}
+
+pub fn main() !void {
+    if (!glfw.init(.{})) {
+        glfw_log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        return error.GLFWInitFailed;
+    }
+    defer glfw.terminate();
+
+    // Create our window, specifying that we want to use OpenGL.
+    const window = glfw.Window.create(640, 480, "mach-glfw + OpenGL", null, null, .{
+        .context_version_major = gl.info.version_major,
+        .context_version_minor = gl.info.version_minor,
+        .opengl_profile = .opengl_core_profile,
+        .opengl_forward_compat = true,
+    }) orelse {
+        glfw_log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        return error.CreateWindowFailed;
+    };
+    defer window.destroy();
+
+    // Make the window's OpenGL context current.
+    glfw.makeContextCurrent(window);
+    defer glfw.makeContextCurrent(null);
+
+    // Enable VSync to avoid drawing more often than necessary.
+    glfw.swapInterval(1);
+
+    // Make the OpenGL procedure table current.
+    gl.makeProcTableCurrent(&gl_procs);
+    defer gl.makeProcTableCurrent(null);
+
+    while (true) {
+        glfw.pollEvents();
+        if (window.shouldClose()) break;
+        {}
+        window.swapBuffers();
+    }
+}
+
+// pub fn main() !void {
+//     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+//     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+//     // stdout is for the actual output of your application, for example if you
+//     // are implementing gzip, then only the compressed bytes should be sent to
+//     // stdout, not any debugging messages.
+//     const stdout_file = std.io.getStdOut().writer();
+//     var bw = std.io.bufferedWriter(stdout_file);
+//     const stdout = bw.writer();
+
+//     try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+//     try bw.flush(); // don't forget to flush!
+// }
+
+// test "simple test" {
+//     var list = std.ArrayList(i32).init(std.testing.allocator);
+//     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+//     try list.append(42);
+//     try std.testing.expectEqual(@as(i32, 42), list.pop());
+// }
